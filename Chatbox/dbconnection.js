98ac72a82349ca3c9c4ab3065a81c0d4b5c1ae0a
Glennvdk <49276613+Glennvdk@users.noreply.github.com>
2019-06-07 10:24:20 +0200

    Delete dbconnection.js

--- Chatbox/dbconnection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = require("bluebird");
-const url = "mongodb+srv://Admin:Admin123@cluster0-bocyg.azure.mongodb.net/chatroom_backbone?retryWrites=true&w=majority"
-const connect = mongoose.connect(url, { useNewUrlParser: true });
-module.export = connect;
